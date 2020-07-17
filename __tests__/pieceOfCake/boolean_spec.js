b56728db43aa959afd156dcc30b8bb7ef16f5af4
Tobey <bitsqiu@gmail.com>
2020-07-17 10:32:31 +0800

    [di.qin] fix: boolean_spec

--- __tests__/pieceOfCake/boolean_spec.js
+++ __tests__/pieceOfCake/boolean_spec.js
@@ -1,33 +1,43 @@
-describe('for boolean type', () => {
-  it('should convert to same type then compare for equality operator', () => {
-    const objectLeft = { key: 'value' };
-    const objectRight = { key: 'value' };
+describe("for boolean type", () => {
+  it("should convert to same type then compare for equality operator", () => {
+    const objectLeft = { key: "value" };
+    const objectRight = { key: "value" };
 
     const actual = [
       // eslint-disable-next-line no-self-compare, eqeqeq, yoda
-      1 == 1, '1' == 1, 1 == '1', 0 == false, 0 == null, objectLeft == objectRight, 0 == undefined, null == undefined,
+      1 == 1,
+      "1" == 1,
+      1 == "1",
+      0 == false,
+      0 == null,
+      objectLeft == objectRight,
+      0 == undefined,
+      null == undefined,
     ];
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = [true, true, true, true, false, false, false, true];
     // --end->
 
     expect(actual).toEqual(expected);
   });
 
-  it('should not perform type conversion for strict equal operator', () => {
-    const objectLeft = { key: 'value' };
-    const objectRight = { key: 'value' };
+  it("should not perform type conversion for strict equal operator", () => {
+    const objectLeft = { key: "value" };
+    const objectRight = { key: "value" };
 
     const actual = [
       // eslint-disable-next-line no-self-compare, eqeqeq, yoda
-      3 === 3, 3 === '3', objectLeft === objectRight, null === undefined,
+      3 === 3,
+      3 === "3",
+      objectLeft === objectRight,
+      null === undefined,
     ];
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = [true, false, false, false];
     // --end->
 
     expect(actual).toEqual(expected);
