966a5f7534a85ccf937e1b2278572925f37b2a13
Tobey <bitsqiu@gmail.com>
2020-07-17 12:25:48 +0800

    fixed: variable_spec

--- __tests__/pieceOfCake/variable_spec.js
+++ __tests__/pieceOfCake/variable_spec.js
@@ -1,39 +1,39 @@
-describe('for variable', () => {
-  it('should have function scope for var variable', () => {
+describe("for variable", () => {
+  it("should have function scope for var variable", () => {
     // eslint-disable-next-line vars-on-top, no-var, no-empty
-    for (var i = 0; i <= 5; i += 1) { }
+    for (var i = 0; i <= 5; i += 1) {}
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = 6;
     // --end->
 
     // eslint-disable-next-line block-scoped-var
     expect(i).toEqual(expected);
   });
 
-  it('should have block scope for let and const variable', () => {
+  it("should have block scope for let and const variable", () => {
     // eslint-disable-next-line prefer-const
     let i = 1000;
     // eslint-disable-next-line no-empty, no-shadow
-    for (let i = 0; i <= 5; i += 1) { }
+    for (let i = 0; i <= 5; i += 1) {}
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = 1000;
     // --end->
 
     // eslint-disable-next-line no-undef
     expect(i).toEqual(expected);
   });
 
-  it('should be able to change details of const variable', () => {
-    const constVariable = { name: 'changeit' };
-    constVariable.name = 'new name';
+  it("should be able to change details of const variable", () => {
+    const constVariable = { name: "changeit" };
+    constVariable.name = "new name";
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = "new name";
     // --end->
 
     expect(constVariable.name).toEqual(expected);
